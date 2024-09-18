04ef5d26b464f42c1656a351dedf239b871f8c2a
Will Poole <wp1g16@soton.ac.uk>
2024-09-18 15:26:14 +0100

    mup and hg titration inputs

---
+++ HostGuest/Titrations/trans-4-methylcyclohexanol/repeat_4/-10.696/runNCMC_-10.696.py
@@ -0,0 +1,108 @@
+import numpy as np
+import argparse
+from simtk.openmm.app import *
+from simtk.openmm import *
+from simtk.unit import *
+from openmmtools.integrators import BAOABIntegrator
+import grand
+from sys import stdout
+import glob
+# Load in PDB
+pdb = PDBFile('../../../../bCD-Equiled.pdb')
+lig_pdb = glob.glob('../../*.pdb')
+print(lig_pdb)
+lig_xml = glob.glob('../../*.xml')
+# Add ghost waters,
+pdb.topology, pdb.positions, ghosts = grand.utils.add_ghosts(pdb.topology,
+                                                             pdb.positions,
+                                                             molfile=lig_pdb[0],
+                                                             n=10,
+                                                             pdb='betaCDWithghosts.pdb')
+# Load force field and create system
+ff = ForceField('../../../../bCD_FINAL.xml', 'amber14/tip3p.xml', lig_xml[0])
+system = ff.createSystem(pdb.topology,
+                         nonbondedMethod=PME,
+                         nonbondedCutoff=12.0*angstroms,
+                         switchDistance=10.0*angstroms,
+                         constraints=HBonds)
+
+# Langevin Integrator
+integrator = BAOABIntegrator(298*kelvin, 1.0/picosecond, 0.002*picoseconds)
+
+# Sphere data
+ref_atoms = [{'name': 'C34', 'resname': 'MGO', 'resid': '1'},
+             {'name': 'C7', 'resname': 'MGO', 'resid': '1'}]
+
+sphere_rad = 5.0 * angstrom
+
+
+# Set up NCMC Sphere sampler
+print('Setting up sphere sampler object...')
+ncmc_mover = grand.samplers.NonequilibriumGCMCSphereSampler(system=system, topology=pdb.topology, temperature=298*kelvin, integrator=integrator, excessChemicalPotential=None, adams= -10.696, standardVolume=None, referenceAtoms=ref_atoms, sphereRadius=sphere_rad, nPertSteps=1499, nPropStepsPerPert=50, resname='L02', ghostFile='ncmc-ghost-ligs.txt', log='betaCD.log', dcd='betaCD_raw.dcd', overwrite=True, maxN=1, recordTraj=False) 
+
+
+# Get platform
+platform = Platform.getPlatformByName('CUDA')
+platform.setPropertyDefaultValue('Precision', 'mixed')
+print(f'Running on {platform} platform.')
+# Set up simulation object
+simulation = Simulation(pdb.topology, system, ncmc_mover.integrator, platform)
+simulation.context.setPositions(pdb.positions)
+simulation.context.setVelocitiesToTemperature(298*kelvin)
+simulation.context.setPeriodicBoxVectors(*pdb.topology.getPeriodicBoxVectors())
+
+simulation.reporters.append(StateDataReporter(stdout, 500, step=True, time=True,
+        potentialEnergy=True, temperature=True, density=True, volume=True))
+
+print('Initialising...')
+# Switch off ghosts
+ncmc_mover.initialise(simulation.context, simulation, ghosts)
+ncmc_mover.deleteMoleculesInGCMCSphere()
+
+print('Minimising...')
+simulation.minimizeEnergy(tolerance=0.0001*kilojoule/mole, maxIterations=10000)  # Quick Minimisation
+
+print('Equilibration...')
+for i in range(150):
+    simulation.step(500)
+    ncmc_mover.move(simulation.context, 1)
+
+print("{}/{} equilibration NCMC moves accepted. N = {}".format(ncmc_mover.n_accepted,
+                                                               ncmc_mover.n_moves,
+                                                               ncmc_mover.N))
+
+ncmc_mover.reset() # Reset stats
+print('\nProduction....')
+
+print('Running NCMC...')
+for i in range(2000):
+    simulation.step(500)
+    ncmc_mover.move(simulation.context, 1)
+    ncmc_mover.report(simulation)
+
+
+with open('Insertion_Works.txt', 'w') as fi:
+    for work in ncmc_mover.insert_works:
+        fi.write(f'{work}\n')
+
+with open('Deletion_Works.txt', 'w') as fi:
+    for work in ncmc_mover.delete_works:
+        fi.write(f'{work}\n')
+
+
+# Setup the output files
+# Move ghost waters out of the simulation cell
+trj = grand.utils.shift_ghost_waters(ghost_file='ncmc-ghost-ligs.txt',
+                                     topology='betaCDWithghosts.pdb',
+                                     trajectory='betaCD_raw.dcd')
+
+
+# Write out a trajectory of the GCMC sphere
+grand.utils.write_sphere_traj(radius=sphere_rad,
+                              ref_atoms=ref_atoms,
+                              topology='betaCDWithghosts.pdb',
+                              trajectory='betaCD_raw.dcd',
+                              output='gcmc_sphere.pdb',
+                              initial_frame=True)
+
+
---
+++ MUP1/Titration/09/repeat_3/-20.824/runNCMC_-20.824.py
@@ -0,0 +1,173 @@
+import math
+
+import numpy as np
+import argparse
+from simtk.openmm.app import *
+from simtk.openmm import *
+from simtk.unit import *
+from openmmtools.integrators import BAOABIntegrator
+import grand
+from sys import stdout
+import glob
+
+# Input Files
+pdb = PDBFile('../../../ProteinEquiled.pdb')  # Could be an arg_parser
+lig_pdb = glob.glob('../../*.pdb')
+lig_xml = glob.glob('../../*.xml')
+
+# System Configuration
+nonbondedMethod = PME
+nonbondedCutoff = 1.2*nanometers
+switchDistance=10.0*angstroms
+ewaldErrorTolerance = 0.0005  # Default
+constraints = HBonds
+rigidWater = True  # Default
+hydrogenMass = 2.0*amu
+
+# Integration Options
+dt = 0.004*picoseconds
+temperature = 298*kelvin
+friction = 1.0/picosecond
+"""
+Dont need these for GCMC
+pressure = 1.0*atmospheres
+barostatInterval = 25
+"""
+
+# GCNCMC params
+mu_ex =  None
+volume_per_lig = None
+n_pert = 749
+n_prop = 50
+adams_value = -20.824
+
+
+
+# Get platform
+platform = Platform.getPlatformByName('CUDA')
+platformProperties = {'Precision': 'mixed'}
+platform.setPropertyDefaultValue('Precision', 'mixed')
+print(f'Running on {platform} platform.')
+
+# Prepare Simulation and add ghost molecules for GCMC.
+pdb.topology, pdb.positions, ghosts = grand.utils.add_ghosts(pdb.topology,
+                                                             pdb.positions,
+                                                             molfile=lig_pdb[0],
+                                                             n=20,
+                                                             pdb='MUP1WithGhosts.pdb')
+# Load force field and create system
+ff = ForceField('amber14-all.xml', 'amber14/tip3p.xml', lig_xml[0])
+system = ff.createSystem(pdb.topology,
+                         nonbondedMethod=nonbondedMethod,
+                         nonbondedCutoff=nonbondedCutoff,
+                         switchDistance=switchDistance,
+                         constraints=constraints,
+                         rigidWater=rigidWater,
+                         hydrogenMass=hydrogenMass)
+
+# Langevin Integrator
+integrator = BAOABIntegrator(temperature, friction, 0.002 * picoseconds)
+
+# Sphere data and gcmc stuff
+ref_atoms = [{'name': 'CA', 'resname': 'PHE', 'resid': '57'},
+             {'name': 'CA', 'resname': 'LEU', 'resid': '106'}]
+sphere_rad = 5.5 * angstrom
+
+# Set up NCMC Sphere sampler
+print('Setting up sphere sampler object...')
+ncmc_mover = grand.samplers.NonequilibriumGCMCSphereSampler(system=system, topology=pdb.topology,
+                                                            temperature=temperature,
+                                                            integrator=integrator,
+                                                            excessChemicalPotential=mu_ex,
+                                                            adams=adams_value, standardVolume=volume_per_lig,
+                                                            referenceAtoms=ref_atoms, sphereRadius=sphere_rad,
+                                                            nPertSteps=n_pert, nPropStepsPerPert=n_prop, timeStep=dt,
+                                                            resname='L02',
+                                                            ghostFile='ncmc-ghost-ligs.txt', log='MUP1NCMC.log',
+                                                            rst="checkpoint.rst7",
+                                                            dcd='MUP1NCMCholo_raw.dcd', overwrite=True, recordTraj=False, maxN=1)
+
+# Set up simulation object
+simulation = Simulation(pdb.topology, system, ncmc_mover.integrator, platform, platformProperties=platformProperties)
+simulation.context.setPositions(pdb.positions)
+simulation.context.setVelocitiesToTemperature(298*kelvin)
+simulation.context.setPeriodicBoxVectors(*pdb.topology.getPeriodicBoxVectors())
+
+simulation.reporters.append(StateDataReporter(stdout, 1000, step=True, time=True,
+        potentialEnergy=True, temperature=True, volume=True))
+
+# Switch off ghosts
+ncmc_mover.initialise(simulation.context, simulation, ghosts)
+ncmc_mover.deleteMoleculesInGCMCSphere()
+
+print('Minimising...')
+simulation.minimizeEnergy(tolerance=0.0001*kilojoule/mole, maxIterations=10000)  # Quick Minimisation
+
+print('Equilibration...')
+# Run some MD for equil
+equil_md_steps = math.ceil((1.0 * nanosecond) /(0.002 * picoseconds))
+print(f"Simulating for {equil_md_steps} MD equil steps at 2 fs")
+simulation.step(equil_md_steps)
+
+integrator.setStepSize(dt)
+print(integrator.getStepSize())
+
+moves_since_accepted = 0
+n_acc = 0
+for i in range(150):
+    simulation.step(1000)
+    ncmc_mover.move(simulation.context, 1)
+    if ncmc_mover.n_accepted == n_acc + 1:
+        n_acc += 1
+        moves_since_accepted = 0
+    elif ncmc_mover.n_accepted == n_acc:
+        moves_since_accepted += 1
+
+
+print("{}/{} equilibration NCMC moves accepted. N = {}".format(ncmc_mover.n_accepted,
+                                                               ncmc_mover.n_moves,
+                                                               ncmc_mover.N))
+
+print(f"Simulating for {equil_md_steps} MD equil steps")
+simulation.step(equil_md_steps)
+
+ncmc_mover.reset() # Reset stats
+print('\nProduction....')
+
+print('Running NCMC...')
+n_acc = 0
+for i in range(2000):
+    simulation.step(1000)
+    ncmc_mover.move(simulation.context, 1)
+    ncmc_mover.report(simulation)
+    if ncmc_mover.n_accepted == n_acc + 1:
+        n_acc += 1
+        moves_since_accepted = 0
+    elif ncmc_mover.n_accepted == n_acc:
+        moves_since_accepted += 1
+
+    if moves_since_accepted == 350:
+        break  # Quit after 350 with no accepted moves
+
+
+# Setup the output files
+# Move ghost waters out of the simulation cell
+trj = grand.utils.shift_ghost_waters(ghost_file='ncmc-ghost-ligs.txt',
+                                     topology='MUP1WithGhosts.pdb',
+                                     trajectory='MUP1NCMCholo_raw.dcd')
+
+# Recentre the trajectory on a particular residue
+#trj = grand.utils.recentre_traj(t=trj, resname='ILE', resid=221)
+
+# Align the trajectory to the protein
+grand.utils.align_traj(t=trj, output='MUP1NCMCholo.dcd', reference="MUP1WithGhosts.pdb")
+
+# Write out a trajectory of the GCMC sphere
+grand.utils.write_sphere_traj(radius=sphere_rad,
+                              ref_atoms=ref_atoms,
+                              topology='MUP1WithGhosts.pdb',
+                              trajectory='MUP1NCMCholo.dcd',
+                              output='gcmc_sphere.pdb',
+                              initial_frame=True)
+
+
