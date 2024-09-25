9985869f8c9f0a298806f5c8fece8a17a680d0a7
WillGPoole <wp1g16@soton.ac.uk>
2024-09-25 10:16:38 +0100

    Host guest titrations added

---
+++ HostGuest/Titrations/p-cresol/repeat_1/-5.211807749554555/postPro.py
@@ -0,0 +1,39 @@
+import numpy as np
+import argparse
+from simtk.openmm.app import *
+from simtk.openmm import *
+from simtk.unit import *
+from openmmtools.integrators import BAOABIntegrator
+import grand
+from sys import stdout
+import glob
+# Load in PDB
+pdb = PDBFile('../../bCD-Equiled.pdb')
+lig_pdb = glob.glob('../../4-*.pdb')
+print(lig_pdb)
+lig_xml = glob.glob('../../4-*.xml')
+
+# Sphere data
+ref_atoms = [{'name': 'C34', 'resname': 'MGO', 'resid': '1'},
+             {'name': 'C7', 'resname': 'MGO', 'resid': '1'}]
+
+sphere_rad = 5.0 * angstrom
+
+
+# Setup the output files
+# Move ghost waters out of the simulation cell
+trj = grand.utils.shift_ghost_waters(ghost_file='ncmc-ghost-ligs.txt',
+                                     topology='betaCDWithghosts.pdb',
+                                     trajectory='betaCD_raw.dcd',
+                                     output='betaCD_shifted.dcd')
+
+
+# Write out a trajectory of the GCMC sphere
+grand.utils.write_sphere_traj(radius=sphere_rad,
+                              ref_atoms=ref_atoms,
+                              topology='betaCDWithghosts.pdb',
+                              trajectory='betaCD_raw.dcd',
+                              output='gcmc_sphere.pdb',
+                              initial_frame=True)
+
+
---
+++ HostGuest/Titrations/p-cresol/repeat_4/-14.245026191300425/postpro.py
@@ -0,0 +1,18 @@
+import numpy as np
+import argparse
+from simtk.openmm.app import *
+from simtk.openmm import *
+from simtk.unit import *
+from openmmtools.integrators import BAOABIntegrator
+import grand
+from sys import stdout
+import glob
+
+# Move ghost waters out of the simulation cell
+trj = grand.utils.shift_ghost_waters(ghost_file='ncmc-ghost-ligs.txt',
+                                     topology='betaCDWithghosts.pdb',
+                                     trajectory='betaCD_aligned.dcd',
+                                     output='betaCD_shifted.dcd')
+
+
+
